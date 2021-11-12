94275e95d55b5d52d9b0c4603c4367f264ef6643
Amit Bhandal <amitbhandal@Amits-MacBook-Air.local>
2021-11-11 18:19:32 -0800

    fixed prop

--- src/components/Balance/Balance.js
+++ src/components/Balance/Balance.js
@@ -8,7 +8,7 @@ import { Grid } from '@material-ui/core';
 
 const Balance = () => {
   return (
-    <Grid container spacing={0} justify="center">
+    <Grid container spacing={0} justifyContent="center">
         <Grid item component={Card} xs={12} md={3} className="">
             <Card sx={{ }}>
                 <CardHeader
