2943a52e7f585cbead4b6f98f4b7757ba55af1d7
Amit Bhandal <amitbhandal@Amits-MacBook-Air.local>
2021-11-30 10:12:05 -0800

    Added Balance component in the 'My Bets' page

--- src/pages/UserBets.js
+++ src/pages/UserBets.js
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { Button } from "@mui/material";
 import { AUTHENTICATED } from "../utils/firebase";
+import { Balance } from "../components/Balance/Balance";
 import { useAuthStateContext } from "../components/AuthStateContext";
 import MonetizationOnIcon from '@mui/icons-material/MonetizationOn';
 
@@ -11,19 +12,19 @@ const UserBets = () => {
 
   return (
     <div>
-      <nav>
       <RouterLink to="/">
         <Button variant="contained">
           <MonetizationOnIcon/>
           Place Bets
         </Button>
-      </RouterLink>       
+      </RouterLink>   
+      <Balance value={userData.balance} />
+      <nav>    
       </nav>
       {isAuthenticated && (
         <div>
           <div>Name: {authState.user.displayName}</div>
           <div>Email: {authState.user.email}</div>
-          <div>Balance: {userData.balance}</div>
           {<div>Placed Bets: {JSON.stringify(userBets)}</div>}
           {<div>Games: {JSON.stringify(userGames)}</div>}
           <Button
